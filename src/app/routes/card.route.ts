030df86e7211c48cb0a8e8610856fd502ff20144
nayyaung9 <naygit.developer@gmail.com>
2021-01-31 18:54:56 +0630

    feat: card module made to work -MVP

--- src/app/routes/card.route.ts
+++ src/app/routes/card.route.ts
@@ -2,4 +2,10 @@ const cardController = require("../controllers/card.controller");
 
 module.exports = (app: any) => {
   app.route("/api/card").post(cardController.createNewCard);
+  app.route("/api/cards").get(cardController.fetchAllCards);
+  app
+    .route("/api/card/:id")
+    .get(cardController.fetchCardDetail)
+    .put(cardController.updateCard)
+    .delete(cardController.deleteCard);
 };
